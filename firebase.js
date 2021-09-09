11947e80d2ad4c201420f195f4c2b6076d505374
Vikrantbais42 <90373877+Vikrantbais42@users.noreply.github.com>
2021-09-09 11:22:49 +0530

    push code

---
+++ firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC67RKR7Wg3kXMs--hpWsKwt5D5YpRQhys",
+    authDomain: "krishworks-assignment.firebaseapp.com",
+    databaseURL: "https://krishworks-assignment-default-rtdb.firebaseio.com",
+    projectId: "krishworks-assignment",
+    storageBucket: "krishworks-assignment.appspot.com",
+    messagingSenderId: "54186077194",
+    appId: "1:54186077194:web:a912ce255a6e6621bdc3f3",
+    measurementId: "G-5Z2GGVKFNM"
+  };
+  
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
